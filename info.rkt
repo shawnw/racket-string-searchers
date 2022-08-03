2bf641ea8e999349367c4fed02c1cf20f80a616a
Shawn Wagner <shawnw.mobile@gmail.com>
2022-08-03 04:37:21 -0700

    Tweak info.rkt to add typed-racket-lib

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "string-searchers")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base" "typed-racket-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "typed-rackunit-lib"))
 (define scribblings '(("scribblings/string-searchers.scrbl" ())))
 (define pkg-desc "String Search Algorithms")
 (define version "0.5")

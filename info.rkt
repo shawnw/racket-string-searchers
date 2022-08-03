33acfe12cef865181e030e6f1a616774fd4adbf0
Shawn Wagner <shawnw.mobile@gmail.com>
2022-08-03 04:45:25 -0700

    Still more dependency tweaks

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "string-searchers")
 (define deps '("base" "typed-racket-lib" "srfi-lite-lib"))
-(define build-deps '("scribble-lib" "typed-racket-doc" "rackunit-typed"))
+(define build-deps '("scribble-lib" "racket-doc" "typed-racket-doc" "rackunit-typed"))
 (define scribblings '(("scribblings/string-searchers.scrbl" ())))
 (define pkg-desc "String Search Algorithms")
 (define version "0.5")
